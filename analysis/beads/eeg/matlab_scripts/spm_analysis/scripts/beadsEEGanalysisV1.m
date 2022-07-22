0ea6771e73e687042ff15d7f85e4b06aea5bc3e4
christinadelta <cdimitriadou@seerc.org>
2022-07-22 16:22:00 +0100

    updated EEG analysis script for beads

--- analysis/beads/eeg/matlab_scripts/spm_analysis/scripts/beadsEEGanalysisV1.m
+++ analysis/beads/eeg/matlab_scripts/spm_analysis/scripts/beadsEEGanalysisV1.m
@@ -21,7 +21,7 @@
 % GUI. 
 
 % The are two functions that are modified or created specifically for the
-% Beads task. These are:
+% Beads task in the 'utilities' dir. These are:
 
 % 1. createMontage.m -- This function creates a montage (performs channel
 % selection) and re-refernces the signal to the average of all electrodes.
@@ -32,14 +32,75 @@
 % channel exclude it from the createMontage.m function (for more info see
 % the documentation within the createMontage.m file).
 
-% 2. beads_trialdef.m -- this function re-writes the events as as draw
+% 2. beadsTrialdef.m -- this function re-writes the events as as draw
 % choices and urn choices (for the 0.8 & 0.6 conditions). During the
 % recording there was no way to code the triggers as draw and urn choices
 % (because the trigger is sent right after the bead presentation screen is
 % flipped), thus we need to do this change "manually" before epoching. For
 % more info on how this is accomplished, see the documentation of the 
-% beads_trialdef.m file. 
+% beadsTrialdef.m file. 
 
+%% Create required directories and define paths %%
+
+% Add SPM12 to the matlab path if needed:
+% addpath /Users/christinadelta/neuro_software/spm12 % change this to your
+% own SPM12 path
+% savepath
+
+% create spm directories that will be used for storing the output MEEG
+% objects mat files and images. MEEG objects are stored in the output dir,
+% .mat files in the jobs dir and images are stored in directories that are
+% created automatically during conversion of the MEEG objects to .nii
+% files.
+
+basedir     = pwd;
+spmdir      = fullfile(basedir, 'spmDir');
+addpath(genpath(spmdir));
+
+% if output and jobs directories do not exist, create them:
+erpDir      = fullfile(spmdir, 'outerps');
+tfrDir      = fullfile(spmdir, 'outtfrs');
+jobsDir     = fullfile(spmdir, 'jobs');
+
+if ~exist(erpDir, 'dir') && ~exist(tfrDir, 'dir') && ~exist(jobsDir, 'dir')
+    mkdir(erpDir)
+    mkdir(tfrDir)
+    mkdir(jobsDir)
+end
+
+% set data path
+datadir     = '/Users/christinadelta/Desktop/os_data/beads/subs/';  % change to your own data path 
+subs        = dir(fullfile(datadir, '*sub*'));                      % how many subject folders?
+nsubs       = length(subs);                                         % how many subjects?
+
+taskname    = 'beads';
+blocks      = 4;
+conditions  = 2;
+choices     = 2;
+
+%% Preprocessing steps 1 - 8 %%
+
+% these steps are used to preprocess the raw .bdf files, for each
+% participant. Given that we will run evoked analysis and time-frequency
+% analysis, we will low-pass filter the data in two ways, one for evoked
+% analysis and one for TF analysis. Both MEEG objects will be preprocessed
+% up until merging, however, only the ERPs MEEG object will undergo
+% artefact rejection (the last step of preprocessing).
+
+% loop over subjects 
+for sub = 1:nsubs
+    
+    fprintf('loading beads block data\n')  
+    subject = subs(sub).name;
+    subdir  = fullfile(datadir,subject);
+    fprintf('\t reading data from subject %d\n',sub); 
+    
+    % loop over blocks 
+    for block = 1:blocks
+        
+        fprintf('\t\t loading block %d\n\n',block);
+        
+    end % end of blocks loop    
  
-
+end % end of subjects loop
 
