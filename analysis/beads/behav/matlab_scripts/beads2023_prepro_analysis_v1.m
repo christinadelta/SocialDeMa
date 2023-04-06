b9373a0992b35b582acb9b9cc4049d7a5bde362c
christinadelta <pljt159@live.rhul.ac.uk>
2023-04-06 18:51:34 +0100

    updated parameter recovery for model 2 beads

--- analysis/beads/behav/matlab_scripts/beads2023_prepro_analysis_v1.m
+++ analysis/beads/behav/matlab_scripts/beads2023_prepro_analysis_v1.m
@@ -486,7 +486,7 @@ anova_struct        = struct('all_draws', all_draws, 'all_acc', all_acc,'all_ioa
 
 output_struct_two   = runBehavStats(nsubs, anova_struct); % output will be used for plotting 
 
-%% RECOVER PARAMETERS %% 
+%% FOR MODEL FOR MODEL 1 (BETA) %%
 
 % in parameter recover simulate 52 datasets and fit the model N
 % times with a range of Beta parameter values (in model 1) and a range of
@@ -563,8 +563,8 @@ for rep = 1:nReps
             end % end of condition
         end % end of trials loop
         
-        performance(cond)               = mean(fitted_choice);
-        fitted_avsamples(cond)          = mean(fitted_samples);
+        performance(cond)                       = mean(fitted_choice);
+        fitted_avsamples(cond)                  = mean(fitted_samples);
         
         % store the main simulated parameters and samples
         simulated_model1(rep).beta(cond)       = simR.initbeta;
@@ -573,14 +573,107 @@ for rep = 1:nReps
         % store fitted parameters and samples
         fitted_model1(rep).samples(:,cond)     = fitted_samples;
         fitted_model1(rep).beta(cond)          = minParams(1);
-        fitted_model1(rep).correct(cond)       = performance;
-        fitted_model1(rep).avsamples(cond)     = fitted_avsamples;
+        fitted_model1(rep).correct(cond)       = performance(cond);
+        fitted_model1(rep).avsamples(cond)     = fitted_avsamples(cond);
         fitted_model1(rep).ll(cond)            = ll;
         
     end 
 
-
 end % end of model 1 repetitions loop
+
+clear simR fitted_samples fitted_urnchoice fitted_choice
+%% PARAMETER RECOVER FOR MODEL 2 (BETA & COST-SAMPLE) %%
+
+% define parameters for simulations
+% simR.rangeCs        = [-5:0.25:0];
+simR.correct            = 10;
+simR.error              = -10;
+simR.diff               = -20;
+simR.freeparams         = 2; 
+
+% deal with betas
+beta_bounds             =[0 30];        % maximum and minimum value of beta?
+cs_bounds               = [-4 0]; % maximum and minimum cost to sample
+nbins                   = [25 20];
+allbetas                = linspace(beta_bounds(1), beta_bounds(2), nbins(1)+1);
+allCs                   = linspace(cs_bounds(1), cs_bounds(2), nbins(2)+1);
+
+% RUN PARAMETER RECOVERY FORM MODEL 2
+for cs = 1:length(allCs)
+
+    simR.initsample = allCs(cs); % extract cost-sample for this loop
+
+    for beta = 1:length(allbetas)
+
+        simR.initbeta = allbetas(beta);
+
+        % simulate dataset of 52 sequences/trials (26 easy and 26 difficult ones)
+        simoutput = simBeadsData(simvars,simR);
+
+        % store the main simulated parameters and samples
+        simulated_model2(beta).samples          = simoutput.simdraws;
+        simulated_model2(beta).avsamples        = simoutput.avsamples;
+
+        for cond = 1:conditions
+
+            cond_simsequence                                        = simoutput.simsequences{1,cond};
+            cond_simvecs                                            = simoutput.simchoicevec{1,cond};
+            simR.thisq                                              = simvars.qvals(cond);
+    
+            % what is the simulated urntype?
+            simurntype                                         = simoutput.simurns(:,cond);
+    
+            for u = 1:length(simurntype)
+                if simurntype(u) == 0 % if green urn switch index coding
+                    seq_ones                                = find(cond_simsequence(u,:) == 1);
+                    seq_twos                                = find(cond_simsequence(u,:) == 2);
+                    cond_simsequence(u,seq_ones)             = 2;
+                    cond_simsequence(u,seq_twos)             = 1;
+                end 
+            end
+    
+            % recode 2s to 0s for backward induction 
+            cond_simsequence(find(cond_simsequence==2))       = 0;
+    
+            
+            % now that we have simulated sequences and responses, fit the model
+            % with the simulated R struct
+            [minParams,ll,Qsad,cprob,fitted_samples,fitted_urnchoice] = bayesbeads_b(cond_simsequence, cond_simvecs, simR);
+    
+            % compute fitted/recovered performance (to be used for statistical
+            % analysis 
+            for t = 1:size(fitted_urnchoice,1)
+                
+                if (fitted_urnchoice(t) == 1 & simurntype(t,1) == 1) | (fitted_urnchoice(t) == 2 & simurntype(t,1) == 0)
+                    fitted_choice(t) = 1;
+                else
+                    fitted_choice(t) = 0;
+                end % end of condition
+            end % end of trials loop
+
+            performance(cond)                       = mean(fitted_choice);
+            fitted_avsamples(cond)                  = mean(fitted_samples);
+            
+            % store the main simulated parameters and samples
+            simulated_model2(beta).beta(cond)       = simR.initbeta;
+            simulated_model2(beta).correct(cond)    = simoutput.performance(cond);
+            simulated_model2(beta).cs(cond)         = simR.initsample;
+    
+            % store fitted parameters and samples
+            fitted_model2(beta).samples(:,cond)     = fitted_samples;
+            fitted_model2(beta).beta(cond)          = minParams(2);
+            fitted_model2(beta).cs(cond)            = minParams(1);
+            fitted_model2(beta).correct(cond)       = performance(cond);
+            fitted_model2(beta).avsamples(cond)     = fitted_avsamples(cond);
+            fitted_model2(beta).ll(cond)            = ll;
+
+        end % end of conditions loop
+
+    end % end of betas loop
+
+    allsim_models2{1,cs} = simulated_model2;
+    allfit_models2{1,cs} = fitted_model2;
+end % end of Cs loop
 
 
 %% COMPARE & SELECT MODELS %%
@@ -601,6 +694,7 @@ for sub = 1:nsubs
 
 
 end % end of subjects loop
+
 
 %%  PREP EEG DATA %%
 
